6b847839c39f829b0acfb417931d3ca403a4a87a
Justin Tracey <j2tracey@gmail.com>
2024-01-03 17:57:59 -0500

    reflow doc comment

--- src/basic_types.rs
+++ src/basic_types.rs
@@ -1,6 +1,8 @@
 use std::fmt::{Display, Formatter};
 
-/// A [Pointer](https://weechat.org/files/doc/devel/weechat_relay_protocol.en.html#object_pointer) as provided by the WeeChat relay.
+/// A
+/// [Pointer](https://weechat.org/files/doc/devel/weechat_relay_protocol.en.html#object_pointer)
+/// as provided by the WeeChat relay.
 ///
 /// Note that while these pointers are probably parsed into an actual machine pointer type on the
 /// relay side, the protocol treats them as strings with particular constraints. This means that
